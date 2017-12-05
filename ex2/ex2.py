d4b89f3a02897543ecf38f71e3e4beea0f3e4687
vagosdim <cse32434@cs.uoi.gr>
2017-12-05 22:39:23 +0200

    implemented 2PC

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,3 +1,8 @@
+# Name: Dimoulis Evaggelos
+# AM: 2434
+# email: cse32434@cs.uoi.gr
+# Cooperating with : Alexandros Giochalas 
+
 #
 # Event-driven code that behaves as either a client or a server
 # depending on the argument.  When acting as client, it connects 
@@ -22,9 +27,15 @@ decision = ''
 vote = ''
 state = 'INIT'
 log = []
+receivedDecision = False
 receivedVotes = 0
 receivedCommits = 0
+requestSendTime = 4
+TIMEOUT0 = 3
+TIMEOUT1 = 5
+
 timeout = False
+requestArrived = False
 
 def parse_args():
 	usage = """usage: %prog [options] process number [hostname]:port
@@ -71,7 +82,7 @@ class Peer(Protocol):
 
 	def connectionMade(self):
 		
-		global NODES_CONNECTED,peerList
+		global NODES_CONNECTED,peerList,startTime
 		self.connected = True
 		
 		try:
@@ -80,8 +91,25 @@ class Peer(Protocol):
 			print e.args[0]
 		NODES_CONNECTED += 1
 		if(NODES_CONNECTED == 2 and self.no == 0):
-			reactor.callLater(3, self.sendRequest)
-			#reactor.callLater(2, self.sendUpdate)
+			self.writeToLog('START_2PC\n')
+			reactor.callLater(requestSendTime, self.sendRequest)
+		elif(NODES_CONNECTED == 2 and self.no != 0):
+			self.writeToLog('INIT\n')
+			startTime = time.time()
+			self.waitForRequest()
+	
+	def waitForRequest(self):
+		
+		global startTime,timeout,requestArrived
+		if(time.time()-startTime > TIMEOUT1 and requestArrived == False):
+			print('timeout')
+			timeout = True
+			self.writeToLog('VOTE_ABORT')
+		else:
+			if(requestArrived == False):
+				reactor.callLater(0.01,self.waitForRequest)
+			else:
+				return
 
 	def multicast(self,data):
 		for peer in peerList:					
@@ -89,11 +117,10 @@ class Peer(Protocol):
 		
 	def sendRequest(self):
 		
-		global startTime,MAX_MESSAGES,peerList,timeout
+		global startTime,MAX_MESSAGES,peerList,timeout,requestSendTime
 		timeout = False
 		
-		if(self.updateCounter == 3):	
-			#self.waitForVotes()
+		if(self.updateCounter == MAX_MESSAGES+1):	
 			return		
 
 		try:
@@ -106,12 +133,12 @@ class Peer(Protocol):
 		self.updateCounter += 1
 		startTime = time.time()
 		self.waitForVotes()
-		#if self.connected == True:
-			#reactor.callLater(5, self.sendRequest)
 
 	def makeDecision(self):
-		global receivedCommits,decision
-		if(receivedCommits == 2):
+		global receivedCommits,decision,vote
+		vote = self.makeVote()
+		print('Coordinator vote: ',vote)
+		if(receivedCommits == 2 and vote == 'VOTE_COMMIT'):
 			decision = 'GLOBAL_COMMIT'
 		else:
 			decision = 'GLOBAL_ABORT'
@@ -119,30 +146,28 @@ class Peer(Protocol):
 		self.writeToLog(decision)
 		self.multicast(decision)
 			
-
 	def waitForVotes(self):
 
-		global receivedVotes,receivedCommits,startTime,timeout
+		global receivedVotes,receivedCommits,startTime,timeout,TIMEOUT0
 		
 		if(receivedVotes < 2):
-			if(time.time()-startTime > 3):
+			if(time.time()-startTime > TIMEOUT0):
 				timeout = True
 				print('TIMEOUT.Answers not received within 3 seconds')
 				state = 'GLOBAL_ABORT'
 				self.writeToLog(state)
 				self.multicast(state)
-				reactor.callLater(3, self.sendRequest)
+				self.sendRequest()
 			else:
-				reactor.callLater(2, self.waitForVotes)
+				reactor.callLater(0.01, self.waitForVotes)
 		else:
 			self.makeDecision()
 			receivedVotes = 0
 			receivedCommits = 0
-			reactor.callLater(3, self.sendRequest)
+			reactor.callLater(requestSendTime, self.sendRequest)
 		return
 
 	def sendState(self,data):
-		#global 
 		print "send current state"
 		try:
 			self.transport.write(str(data)+',Node'+str(self.no))
@@ -157,52 +182,78 @@ class Peer(Protocol):
 			fields = data.split('\n')
 			data = fields[0]
 		log.append(data)
-		print(log)
 		state = data
 		f.close()
 		return
 
 	def makeVote(self):
-		decision = random.randint(0,1)
+		decision = random.randint(0,4)
 		if(decision == 0):
-			return 'VOTE_COMMIT'
-		else:
 			return 'VOTE_ABORT'
+		else:
+			return 'VOTE_COMMIT'
 		
+		
+	def waitForDecision(self):
+		global startTime,receivedDecision
+		if(receivedDecision == False):
+			if(time.time()-startTime > TIMEOUT1):
+				if(self.no == 1):
+					peerList[1].transport.write('DECISION_REQUEST')
+				else:
+					peerList[0].transport.write('DECISION_REQUEST')
+			else:
+				reactor.callLater(0.01,self.waitForDecision)	
+				 
 		
 	def dataReceived(self, data):
-		global vote,state,log,startTime,receivedCommits,receivedVotes,timeout
+		global vote,state,log,startTime,receivedCommits,receivedVotes
+		global timeout,requestArrived,receivedDecision
+
+		tokens = data.split(',')
+		if(data == 'DECISION_REQUEST'):
+			state = log[len(log)-1]
+			if(state == 'GLOBAL_COMMIT'):
+				self.transport.write('GLOBAL_COMMIT')
+			elif(state == 'GLOBAL_ABORT' or state == 'INIT'):
+				self.transport.write('GLOBAL_ABORT')
+			else:
+				return							
+		if(timeout == True and tokens[0] == 'VOTE_REQUEST'):
+			timeout = False
+			startTime = time.time()
+			self.waitForRequest()
+			return
 		if(timeout == True):
 			receivedCommits = 0
 			receivedVotes = 0
 			return
-			 
+	 
 		print('Received: '+data)
 		if(data == '' or data == '<Connection up>'):
 			return
 
-		tokens = data.split(',')
 		if(tokens[0] == 'VOTE_REQUEST'):
+			requestArrived = True
+			receivedDecision = False
 			vote = self.makeVote()
 			self.writeToLog(vote)
-			reactor.callLater(2,self.sendState,state)
-			#if(vote == 'VOTE_COMMIT'):				
-			#	startTime = time.time()
-		#		self.waitFunction()
+			reactor.callLater(0.5,self.sendState,state)
+			if(vote == 'VOTE_COMMIT'):
+				startTime = time.time()
+				self.waitForDecision()	
 		elif(tokens[0] == 'GLOBAL_COMMIT' or tokens[0] == 'GLOBAL_ABORT'):
+			print('\n\n')
 			self.writeToLog(tokens[0]+'\n')
+			requestArrived = False
+			receivedDecision = True
+			startTime = time.time()
+			self.waitForRequest()
 		elif(tokens[0] == 'VOTE_COMMIT'):
 			receivedCommits += 1
 			receivedVotes += 1
 		elif(tokens[0] == 'VOTE_ABORT'):
 			receivedVotes += 1
-		#if(tokens[0] == 'DECISION_REQUEST'):
-		#	if(state == 'GLOBAL_COMMIT'):
-		#		self.sendAck(state)
-		#	elif(state == 'GLOBAL_ABORT' or state == 'INIT'):
-		#		self.sendAck(state)
-		#	else:
-		#		return
 			
 
 	def connectionLost(self, reason):
